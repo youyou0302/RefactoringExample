41030a5b3a0086bc5f2e3088f3af4d6f30bc752b
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 12:17:28 +0900

    [기능] - html 버전 추가

--- statemnet.js
+++ statemnet.js
@@ -1,7 +1,25 @@
 import createStatementData from "./createStatementData.js";
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><ht>좌석 수</th><th>금액</th><tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amont)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumnCredits}</em>점</p>\n`;
+  return result;
 }
 
 function renderPlainText(data) {
