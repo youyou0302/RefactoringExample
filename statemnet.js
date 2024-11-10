0563f13bff6f10fe603fe9a049eb9f40c727c669
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 11:36:20 +0900

    [문장 슬라이드하기] - volumeCredits 변수를 선언하는 문장을 반복문 바로 앞으로 이동

--- statemnet.js
+++ statemnet.js
@@ -1,6 +1,5 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -10,6 +9,7 @@ function statement(invoice, plays) {
     }석}\n)`;
     totalAmount += amountFor(perf);
   }
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(pref);
   }
