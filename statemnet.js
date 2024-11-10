12aed874d12ba490eee1f421aec2d79167f08580
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 11:59:30 +0900

    [함수 옮기기] - 기존에 playFor 사용하던 부분 삭제

--- statemnet.js
+++ statemnet.js
@@ -18,7 +18,7 @@ function statement(invoice, plays) {
 function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} ${
+    result += ` ${perf.play}.name}: ${usd(amountFor(perf))} ${
       perf.audience
     }석}\n)`;
   }
@@ -28,7 +28,7 @@ function renderPlainText(data, plays) {
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -43,19 +43,15 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
     }
     return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
   }
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
