4ad80580d641e62e23d40189f238162fe07a517c
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 14:48:09 +0900

    [함수 옮기기] - amountFor를 계산기에 넣기

--- createStatementData.js
+++ createStatementData.js
@@ -23,25 +23,8 @@ export default function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
 
   function volumeCreditsFor(aPerformance) {
@@ -65,5 +48,27 @@ class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performances = aPerformance;
     this.aPlay = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (this.audience > 30) {
+          result += 1000 * (this.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (this.audience > 20) {
+          result += 10000 + 500 * (this.audience - 20);
+        }
+        result += 300 * this.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
   }
 }
