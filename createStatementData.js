edeadd2648572ef3eccd98d7a1d2ca0ecf7e7f0a
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 14:58:19 +0900

    [생성자를 팩터리 함수로 바꾸기] - createPerformanceCalculator

--- createStatementData.js
+++ createStatementData.js
@@ -40,8 +40,19 @@ export default function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
 }
+
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
 
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
