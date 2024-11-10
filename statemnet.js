29fb012ecffc57663184aae054a434eea289cc33
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 11:15:07 +0900

    [함수 추출하기] - 중간에 case문 amountFor 함수로 추출

--- statemnet.js
+++ statemnet.js
@@ -10,8 +10,25 @@ function statement(invoice, playus) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play);
 
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명 마다 추가 포인트를 제공한다.
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += ` ${play.name}: ${format(thisAmount / 100)} ${
+      perf.audience
+    }석}\n)`;
+    totalAmount += thisAmount;
+  }
+  result += `총액: ${format(totalAmount / 100)}\n}`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+
+  function amountFor(perf, play) {
+    let thisAmount = 0;
     switch (play.type) {
       case "tragedy": // 비극
         thisAmount = 40000;
@@ -29,19 +46,6 @@ function statement(invoice, playus) {
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`);
     }
-
-    // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명 마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-    // 청구 내역을 출력한다.
-    result += ` ${play.name}: ${format(thisAmount / 100)} ${
-      perf.audience
-    }석}\n)`;
-    totalAmount += thisAmount;
+    return thisAmount;
   }
-  result += `총액: ${format(totalAmount / 100)}\n}`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
-  return result;
 }
