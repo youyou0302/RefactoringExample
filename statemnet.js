7c9cf6564daf2457fbe16d536c4822cf4f675987
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 12:05:39 +0900

    [함수 옮기기] - totalAMount, totalVolumnCredits

--- statemnet.js
+++ statemnet.js
@@ -2,6 +2,8 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumnCredits = totalVolumnCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -45,15 +47,31 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+    return result;
+  }
+
+  function totalVolumnCredits(data) {
+    let result = 0;
+    for (let pref of data.performances) {
+      result += pref.volumeCredits;
+    }
+    return result;
+  }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
     result += ` ${perf.play}.name}: ${usd(perf.amount)} ${perf.audience}석}\n)`;
   }
-  result += `총액: ${usd(totalAmount())}\n}`;
-  result += `적립 포인트: ${totalAmountCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount)}\n}`;
+  result += `적립 포인트: ${data.totalVolumnCredits}점\n`;
   return result;
 
   function usd(aNumber) {
@@ -62,21 +80,5 @@ function renderPlainText(data, plays) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalAmountCredits() {
-    let result = 0;
-    for (let pref of data.performances) {
-      result += pref.volumeCredits;
-    }
-    return result;
   }
 }
