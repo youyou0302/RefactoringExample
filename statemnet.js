a029ba9450f29add3b97775513c60ed081c11cb8
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 11:57:03 +0900

    [함수 옮기기] - play 가져오기

--- statemnet.js
+++ statemnet.js
@@ -6,7 +6,12 @@ function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
