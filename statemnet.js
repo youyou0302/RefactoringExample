9f78c8e48c6e5f4d6235967f15c4e7fed2c01dc8
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 11:25:38 +0900

    [함수 선언 바꾸기]

--- statemnet.js
+++ statemnet.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -27,7 +27,7 @@ function statement(invoice, plays) {
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
     switch (playFor(aPerformance).type) {
       case "tragedy": // 비극
