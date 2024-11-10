52d08be78f57c5374c801a17a944785b8613da7d
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 12:02:21 +0900

    [함수 옮기기] - volumnCreditsFor

--- statemnet.js
+++ statemnet.js
@@ -8,6 +8,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -36,6 +37,14 @@ function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -46,14 +55,6 @@ function renderPlainText(data, plays) {
   result += `총액: ${usd(totalAmount())}\n}`;
   result += `적립 포인트: ${totalAmountCredits()}점\n`;
   return result;
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
@@ -74,7 +75,7 @@ function renderPlainText(data, plays) {
   function totalAmountCredits() {
     let result = 0;
     for (let pref of data.performances) {
-      result += volumeCreditsFor(pref);
+      result += pref.volumeCredits;
     }
     return result;
   }
