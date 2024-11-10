e8cbd9fd4a524a6225c2170787e88c56be8b9223
YOUSEUNGHAK <hakyouyouhi@gmail.com>
2024-11-10 11:33:23 +0900

    [함수 선언 바꾸기] - format -> usd

--- statemnet.js
+++ statemnet.js
@@ -7,12 +7,12 @@ function statement(invoice, plays) {
     volumeCredits = volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} ${
       perf.audience
     }석}\n)`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n}`;
+  result += `총액: ${usd(totalAmount)}\n}`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
@@ -50,11 +50,11 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format;
+    }).format(aNumber / 100);
   }
 }
